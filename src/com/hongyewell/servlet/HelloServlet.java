a1d1e4ae9091d2f886823a7c7a73d3ac9ecae3a1
yeye <yeye@yeye-PC>
2015-08-19 15:05:25 +0800

    Hello servlet

---
+++ src/com/hongyewell/servlet/HelloServlet.java
@@ -0,0 +1,30 @@
+package com.hongyewell.servlet;
+
+import java.io.IOException;
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+/**
+* @className:HelloServlet.java
+* @classDescription:
+* @author:yeye
+* @createTime:2015年8月19日 下午2:40:47
+*/
+public class HelloServlet extends GenericServlet {
+
+	/**
+	* TODO
+	*/
+	private static final long serialVersionUID = 6118145983235484612L;
+
+	@Override
+	public void service(ServletRequest req, ServletResponse resp) throws ServletException, IOException {
+ 
+		resp.getOutputStream().write("hello servlet!!!!".getBytes());
+		
+	}
+
+	
+}
